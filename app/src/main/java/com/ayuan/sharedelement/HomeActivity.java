581690c118c9d4ec478301333095718999c934ea
AYuan <lzy2118053626@hotmaill.com>
2020-03-06 19:08:39 +0800

    给Activity加了透明效果

--- app/src/main/java/com/ayuan/sharedelement/HomeActivity.java
+++ app/src/main/java/com/ayuan/sharedelement/HomeActivity.java
@@ -35,6 +35,5 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        supportFinishAfterTransition();
     }
 }
